6c6d7719ba9869c3e68780c164d24f383cf0ffdb
Acutapugione <acuta.pugione@gmail.com>
2023-03-08 20:34:35 +0200

    init

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from app import app
+import time
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return { "my_data" : str(app) }
+    # return f'Current weekday is {time.strftime("%A")}'
+
+
+@app.route('/qwerty', methods=['POST'])
+def post():
+    # return "Hello, World!"
+    return "Well done"
+
+@app.route('/qwerty', methods=['GET'])
+def show_numbs():
+    return f"Сума від 3 до 10: {sum(range(3, 11))}"
